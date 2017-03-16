d85e9e2775dfde5c69fd82eb5561162cf04dc08e
William  Ye <wy733@pvhs-lab112-d06.pvsd.org>
2017-03-16 10:32:26 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+    /**
+     * The main method in this class checks the Card operations for consistency.
+     *	@param args is not used.
+     */
+
+    public static void main(String[] args) {
+    Card a = new Card("A","Spades",14);
+        System.out.println(a.toString());
+    }
+}
